a716a6d6e0de75d1176061899cdefd0cf5b67ed2
Gianluca Fiasco <gianluca.fiasco@servicenow.com>
2019-10-13 22:54:59 +0200

    server homepage

--- TVHm3u/server.py
+++ TVHm3u/server.py
@@ -8,8 +8,24 @@ M3U = TVHm3u(TVH_HOST, TVH_PORT, TVH_USER, TVH_PASS)
 
 class web_server(BaseHTTPRequestHandler):
 
+    def usage(self):
+        html = '<html><body>'
+        html += f'<h1>TVHm3u HOME PAGE {TVH_HOST} on {TVH_PORT}/tcp</h1>'
+        html += '<h2>Entry Points:</h1>'
+        html += '<p><b>/playlist</b> will return m3u file list of channels</p>'
+        html += '<p><b>/xmltv</b> will return the EPG</p>'
+        html += '<p><b>/</b> will print this message</p>'
+        html += '</body></html>'
+        return str.encode(html)
+
     def do_GET(self):
-        if self.path == '/playlist':
+
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(self.usage())
+        elif self.path == '/playlist':
             try:
                 response = M3U.get_m3u()
                 self.send_response(200)
